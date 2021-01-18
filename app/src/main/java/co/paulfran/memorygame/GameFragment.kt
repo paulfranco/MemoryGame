9de416a57296fe2b0caed411a01437b3f455cb68
Paul Franco <mail@paulfran.co>
2021-01-17 18:27:38 -0800

    populate recyclerview with tiles

--- app/src/main/java/co/paulfran/memorygame/GameFragment.kt
+++ app/src/main/java/co/paulfran/memorygame/GameFragment.kt
@@ -1,15 +1,31 @@
 package co.paulfran.memorygame
 
+import android.content.Context
+import android.graphics.Color
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
 class GameFragment : Fragment() {
 
+    interface GameFragmentListener {
+        fun makeTiles(): ArrayList<TextView>
+    }
+
+    private lateinit var caller: GameFragmentListener
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+
+        if (context is GameFragmentListener) {
+            caller = context
+        }
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -17,6 +33,13 @@ class GameFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         val frag = inflater.inflate(R.layout.fragment_game, container, false)
+
+        val context = activity as Context
+        val recyclerView: RecyclerView = frag.findViewById(R.id.gameRv)
+        recyclerView.layoutManager = GridLayoutManager(context, 4)
+
+        val textViews = caller.makeTiles()
+        recyclerView.adapter = GameRecyclerAdapter(textViews)
 
         return frag
     }
@@ -44,8 +67,11 @@ class GameFragment : Fragment() {
             holder.tileParent.addView(thisTile)
         }
 
+
         override fun getItemCount(): Int = inputData.size
     }
+
+
 
 }
 
