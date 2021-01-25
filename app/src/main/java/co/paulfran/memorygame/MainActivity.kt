5f53f06506154cdd61e87cf92a5439287a758efd
Paul Franco <mail@paulfran.co>
2021-01-25 01:49:16 -0800

    game won status

--- app/src/main/java/co/paulfran/memorygame/MainActivity.kt
+++ app/src/main/java/co/paulfran/memorygame/MainActivity.kt
@@ -4,19 +4,19 @@ package co.paulfran.memorygame
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
-
 import android.widget.Toast
-import androidx.core.os.HandlerCompat.postDelayed
+
 
 
 class MainActivity : AppCompatActivity(), GameFragment.GameFragmentListener {
 
-    val tilesArray: ArrayList<Tile> = ArrayList()
-    var thisIsSecondTap = false
-    lateinit var tile1: Tile
-    lateinit var tile2: Tile
+    private val tilesArray: ArrayList<Tile> = ArrayList()
+    private var thisIsSecondTap = false
+    private lateinit var tile1: Tile
+    private lateinit var tile2: Tile
 
-    var gameIsActive = true
+    private var gameIsActive = true
+    private val foundTiles: ArrayList<Tile> = ArrayList()
 
     override fun makeTiles(): ArrayList<Tile> {
         for (i in 1..16) {
@@ -40,7 +40,7 @@ class MainActivity : AppCompatActivity(), GameFragment.GameFragmentListener {
         if (!gameIsActive || tile.tileStatus == Status.FLIPPED || tile.tileStatus == Status.FOUND) {
             return
         }
-        
+
         tile.tileStatus = Status.FLIPPED
         tile.updateTile()
 
@@ -58,13 +58,20 @@ class MainActivity : AppCompatActivity(), GameFragment.GameFragmentListener {
     }
 
     fun compare() {
-        Toast.makeText(this, "let's compare", Toast.LENGTH_SHORT).show()
         if (tile1.value == tile2.value) {
             tile1.tileStatus = Status.FOUND
             tile2.tileStatus = Status.FOUND
 
             tile1.updateTile()
             tile2.updateTile()
+
+            foundTiles.add(tile1)
+            foundTiles.add(tile2)
+
+            if (foundTiles.size == 16) {
+                // won game
+                Toast.makeText(this, "YOU WON", Toast.LENGTH_LONG).show()
+            }
         } else {
             tile1.tileStatus = Status.UNKNOWN
             tile2.tileStatus = Status.UNKNOWN
