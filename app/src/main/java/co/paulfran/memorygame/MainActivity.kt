4e82304f85789f6c8f8fe6a1959d8ce2110a31f1
Paul Franco <mail@paulfran.co>
2021-01-25 01:40:40 -0800

    update the compare method

--- app/src/main/java/co/paulfran/memorygame/MainActivity.kt
+++ app/src/main/java/co/paulfran/memorygame/MainActivity.kt
@@ -1,10 +1,13 @@
 package co.paulfran.memorygame
 
-import android.graphics.Color
+
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.TextView
+import android.os.Handler
+
 import android.widget.Toast
+import androidx.core.os.HandlerCompat.postDelayed
+
 
 class MainActivity : AppCompatActivity(), GameFragment.GameFragmentListener {
 
@@ -12,6 +15,8 @@ class MainActivity : AppCompatActivity(), GameFragment.GameFragmentListener {
     var thisIsSecondTap = false
     lateinit var tile1: Tile
     lateinit var tile2: Tile
+
+    var gameIsActive = true
 
     override fun makeTiles(): ArrayList<Tile> {
         for (i in 1..16) {
@@ -31,6 +36,11 @@ class MainActivity : AppCompatActivity(), GameFragment.GameFragmentListener {
     }
 
     override fun tileTapped(tile: Tile, index: Int) {
+
+        if (!gameIsActive || tile.tileStatus == Status.FLIPPED || tile.tileStatus == Status.FOUND) {
+            return
+        }
+        
         tile.tileStatus = Status.FLIPPED
         tile.updateTile()
 
@@ -40,12 +50,29 @@ class MainActivity : AppCompatActivity(), GameFragment.GameFragmentListener {
         } else {                // second tap
             tile2 = tile
             thisIsSecondTap = false
-            compare()
+            gameIsActive = false
+            Handler().postDelayed({
+                compare()
+            }, 1000)
         }
     }
 
     fun compare() {
         Toast.makeText(this, "let's compare", Toast.LENGTH_SHORT).show()
+        if (tile1.value == tile2.value) {
+            tile1.tileStatus = Status.FOUND
+            tile2.tileStatus = Status.FOUND
+
+            tile1.updateTile()
+            tile2.updateTile()
+        } else {
+            tile1.tileStatus = Status.UNKNOWN
+            tile2.tileStatus = Status.UNKNOWN
+
+            tile1.updateTile()
+            tile2.updateTile()
+        }
+        gameIsActive = true
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
