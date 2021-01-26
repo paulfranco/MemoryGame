031100a1075c64d9cc6e371433fb9b761bfe08fa
Paul Franco <mail@paulfran.co>
2021-01-26 02:47:12 -0800

    Animate Tiles

--- app/src/main/java/co/paulfran/memorygame/Tile.kt
+++ app/src/main/java/co/paulfran/memorygame/Tile.kt
@@ -1,7 +1,12 @@
 package co.paulfran.memorygame
 
+import android.animation.Animator
+import android.animation.AnimatorListenerAdapter
+import android.animation.ObjectAnimator
 import android.content.Context
 import android.graphics.Color
+import android.view.animation.AccelerateInterpolator
+import android.view.animation.DecelerateInterpolator
 import android.widget.TextView
 import androidx.appcompat.widget.AppCompatTextView
 
@@ -22,20 +27,39 @@ data class Tile(var myContext: Context, var value: Int) : AppCompatTextView(myCo
 
     fun updateTile() {
 
-        when (tileStatus) {
-            Status.UNKNOWN -> {
-                this@Tile.text = " ? "
-                this@Tile.setBackgroundColor(Color.DKGRAY)
+        val objAnime_1 = ObjectAnimator.ofFloat(this, "scaleX", 1f, 0f)
+        val objAnime_2 = ObjectAnimator.ofFloat(this, "scaleX", 0f, 1f)
+
+        objAnime_1.duration = 250
+        objAnime_2.duration = 250
+
+        objAnime_1.interpolator = DecelerateInterpolator()
+        objAnime_2.interpolator = AccelerateInterpolator()
+
+        objAnime_1.addListener(object: AnimatorListenerAdapter() {
+            override fun onAnimationEnd(animation: Animator?) {
+                super.onAnimationEnd(animation)
+                when (tileStatus) {
+                    Status.UNKNOWN -> {
+                        this@Tile.text = " ? "
+                        this@Tile.setBackgroundColor(Color.DKGRAY)
+                    }
+                    Status.FLIPPED -> {
+                        this@Tile.text = this@Tile.value.toString()
+                        this@Tile.setBackgroundColor(Color.DKGRAY)
+                    }
+                    Status.FOUND -> {
+                        this@Tile.text = " :) "
+                        this@Tile.setBackgroundColor(Color.GREEN)
+                    }
+                }
+                objAnime_2.start()
             }
-            Status.FLIPPED -> {
-                this@Tile.text = this@Tile.value.toString()
-                this@Tile.setBackgroundColor(Color.DKGRAY)
-            }
-            Status.FOUND -> {
-                this@Tile.text = " :) "
-                this@Tile.setBackgroundColor(Color.GREEN)
-            }
-        }
+        })
+
+        objAnime_1.start()
+
+
     }
     
 }
